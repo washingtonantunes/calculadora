6e3b661d8629cfb9a85e4a642b4711b9702f393c
Washington Antunes <washington.cead@gmail.com>
2022-03-21 21:17:31 -0300

    implementação das classes: 'Botao' e 'Display'

--- src/br/com/washington/calc/visao/Calculadora.java
+++ src/br/com/washington/calc/visao/Calculadora.java
@@ -1,5 +1,36 @@
 package br.com.washington.calc.visao;
 
-public class Calculadora {
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class Calculadora extends JFrame {
+
+	public Calculadora() {
+		
+		organizarLayout();
+		
+		setSize(232, 322);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+	
+	private void organizarLayout() {
+		setLayout(new BorderLayout());
+		
+		Display display = new Display();
+		add(display, BorderLayout.NORTH);
+		display.setPreferredSize(new Dimension(233, 60));
+		
+		Teclado teclado = new Teclado();
+		add(teclado, BorderLayout.CENTER);
+		
+	}
+
+	public static void main(String[] args) {
+		new Calculadora();
+	}
 }
